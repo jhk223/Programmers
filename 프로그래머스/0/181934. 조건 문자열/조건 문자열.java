dba821c3df92235218f4997cb82fd52940bcedd7
jhk223 <94943289+jhk223@users.noreply.github.com>
2024-07-31 12:21:34 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+         if (ineq.equals(">") && eq.equals("=")) {
+            return n >= m ? 1 : 0;
+        } else if (ineq.equals("<") && eq.equals("=")) {
+            return n <= m ? 1 : 0;
+        } else if (ineq.equals(">") && eq.equals("!")) {
+            return n > m ? 1 : 0;
+        } else if (ineq.equals("<") && eq.equals("!")) {
+            return n < m ? 1 : 0;
+        } else {
+            return 0;
+        }
+    }
+}
