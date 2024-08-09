cfc9d3d6a8102c53d366b5650ca5157f553d770e
jhk223 <94943289+jhk223@users.noreply.github.com>
2024-08-09 12:56:48 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        if(my_string.length() > is_prefix.length()){
+            if(my_string.substring(0,is_prefix.length()).equals(is_prefix))
+                answer = 1;
+        }
+        else 
+            answer = 0;
+        return answer;
+    }
+}
