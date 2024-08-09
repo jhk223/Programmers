67d3ab4b0b0c901eb6ddd50921132a6e9e6eec6d
jhk223 <94943289+jhk223@users.noreply.github.com>
2024-08-09 15:41:23 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length+1];
+        for(int i = 0; i < answer.length-1; i++){
+            answer[i] = num_list[i];
+        }
+        if(num_list[num_list.length-1] > num_list[num_list.length-2])
+                answer[answer.length-1] = num_list[num_list.length-1]-num_list[num_list.length-2];
+        else
+             answer[answer.length-1] = num_list[num_list.length-1]*2;       
+        return answer;
+    }
+}
