e1a0e61aaae546ec84fc2828e0915fd502a6c169
jhk223 <94943289+jhk223@users.noreply.github.com>
2024-08-12 17:17:02 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int result;
+        result = Integer.parseInt(n_str);
+        answer = String.valueOf(result);
+        return answer;
+    }
+}
